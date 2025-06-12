458e76a1d64939d884be971d0559e3a3c428b39d
Vida <quiniavidamarie30@gmail.com>
2025-06-12 21:54:28 +0800

    added clear and delete button

--- calculator.js
+++ calculator.js
@@ -1,7 +1,11 @@
 document.addEventListener(("DOMContentLoaded"), () =>{
     let numButtons = document.querySelectorAll(".num-button");
     let numDisplay = document.getElementById("number-text");
+    
+    clearNums();
+    deleteNums();
 
+    //displaying number based on button pressed
     numButtons.forEach((num) =>{
         num.addEventListener(("click"), () =>{
             if(numDisplay.textContent == 0){
@@ -12,7 +16,35 @@ document.addEventListener(("DOMContentLoaded"), () =>{
             }
         });
     });
+
+    //functions
+    function clearNums(){
+        let clearButton = document.getElementById('clear');
+            clearButton.addEventListener(("click"), () =>{
+            numDisplay.textContent = 0;
+        });
+    }
+
+    function deleteNums(){
+        let deleteButton = document.getElementById('delete');
+        deleteButton.addEventListener(("click"), () =>{
+            if(numDisplay.textContent.length == 1){
+                numDisplay.textContent = 0;
+            }
+            else{
+                numDisplay.textContent = (numDisplay.textContent).slice(0, -1);
+            }
+            
+        });
+    }
+
+
 });
+
+
+
+
+
 
 
 
